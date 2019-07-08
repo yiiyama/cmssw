81f0f636469e71234f97a7d2a38999ceed1df9be
Yutaro <yutaro.iiyama@cern.ch>
2019-07-08 17:40:55 +0200

    start modernizing BeamHaloGenerator

--- GeneratorInterface/BeamHaloGenerator/src/PYR.cc
+++ GeneratorInterface/BeamHaloGenerator/src/PYR.cc
@@ -3,6 +3,7 @@
 CLHEP::HepRandomEngine* _BeamHalo_randomEngine;
 
 extern "C" {
+  // C++ function to be called from FORTRAN - at some point we should just port all from FORTRAN to C++
   float bhgpyr_(int* idummy)
   {
     return (float)_BeamHalo_randomEngine->flat();
